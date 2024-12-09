00897e019c850da3637572cb052393ad04a37cd1
Mario Feles dos Santos Junior <mario_feles@live.com>
2024-12-08 23:31:06 -0300

    feat: exemplo polimorfismo

--- src/app/fundamentos/polimorfismo.ts
+++ src/app/fundamentos/polimorfismo.ts
@@ -1,14 +1,41 @@
-import TerminalUtil from "@/util/TerminalUtil";
+import Carro from "@/core/fundamentos/Carro"
+import Ferrari from "@/core/fundamentos/Ferrari"
+import Fusca from "@/core/fundamentos/Fusca"
+import TerminalUtil from "@/util/TerminalUtil"
 
 export default async function polimorfismo() {
-    TerminalUtil.titulo('Polimorfismo')
+    TerminalUtil.titulo("Polimorfismo")
+
+    const [tipoCarro] = await TerminalUtil.selecao(
+        "Tipo de Carro?",
+        ["Ferrari", "Fusca"]
+    )
+
+    const carro: Carro =
+        tipoCarro === 0 ? new Ferrari() : new Fusca()
 
     while (true) {
         TerminalUtil.limpar()
+        TerminalUtil.exibirChaveValor(
+            "Velocidade Maxima: ",
+            `${carro.velocidadeMaxima} km/h`
+        )
 
-        const tipoCarro = await TerminalUtil.selecao('Tipo de Carro?', ['Ferrari', 'Fusca'])
+        TerminalUtil.exibirChaveValor(
+            "Velocidade Atual: ",
+            `${carro.velocidadeAtual} km/h`
+        )
 
-        const continuar = await TerminalUtil.confirmacao('Deseja continuar?')
+        const [opcao] = await TerminalUtil.selecao(
+            "Qual Opção?",
+            ["Acelerar", "Frear"]
+        )
+
+        opcao === 0 ? carro.acelerar() : carro.frear()
+
+        const continuar = await TerminalUtil.confirmacao(
+            "Deseja continuar?"
+        )
         if (!continuar) return
     }
 }
